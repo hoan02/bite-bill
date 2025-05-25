67773bb7ff1649a4bab7af9bf0f951d30c36f97c
HoanIT <lehoan.dev@gmail.com>
2025-05-25 15:13:50 +0700

    fix: remove baseURL in auth-client

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,4 +1,3 @@
-import { env } from "@/env";
 import { createAuthClient } from "better-auth/react";
 export const {
   signIn,
@@ -9,6 +8,4 @@ export const {
   resetPassword,
 } = createAuthClient({
   /** the base url of the server (optional if you're using the same domain) */
-  baseURL: env.BETTER_AUTH_URL,
-
 });
